2edcf3f1ce46a893a09210f9a48c0b1e6ed1183a
Elyas Arkin <(elyas.arkin@hotmail.se)>
2020-09-30 17:44:34 +0200

    update

---
+++ index.js
@@ -0,0 +1,59 @@
+
+
+
+navigator.getBattery().then(function(battery){
+
+    
+    /* This is the initilizing */
+  /*  console.log( battery.level * 100 +"%"); */
+
+
+   /* this is for lestning the Event every time battery gets update */
+
+    battery.addEventListener("levelchange", getUpdateInfo);   
+
+  function getUpdateInfo() {
+
+    var b = Math.round( battery.level *100);
+
+    /*   console.log("Battery level: "+b + " %"); */
+
+      if(b<100 && b>70){
+
+          document.getElementById("div1").style.width="70%";
+          document.getElementById("div1").style.backgroundColor="green";
+          document.getElementById("info").innerHTML=b+"%";
+          document.getElementById("info").style.color="green";
+        
+      }else if(b<70 && b>50) {
+          document.getElementById("div1").style.width = "45%";
+          document.getElementById("div1").style.backgroundColor = "orange";
+          document.getElementById("info").innerHTML = b + "%";
+          document.getElementById("info").style.color = "orange";
+      }else if(b<55 && b>30) {
+          document.getElementById("div1").style.width = "15%";
+          document.getElementById("div1").style.backgroundColor = "grey";
+          document.getElementById("info").innerHTML = b + "%";
+          document.getElementById("info").style.color = "grey";
+      }else if (b<15 && b>1) {
+          document.getElementById("div1").style.width = "5%";
+          document.getElementById("div1").style.backgroundColor = "red";
+          document.getElementById("info").innerHTML = b + "%";
+          document.getElementById("info").style.color = "red";
+      }
+  }
+
+getUpdateInfo();
+
+
+
+
+});      
+
+
+
+
+
+
+
+    
