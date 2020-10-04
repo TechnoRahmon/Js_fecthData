5b427d67717b7c5dde3bcec639cb1dd825e5b4c4
Elyas Arkin <(elyas.arkin@hotmail.se)>
2020-10-04 10:25:35 +0200

    update

---
+++ script.js
@@ -0,0 +1,41 @@
+
+  
+let baseUrl = 'https://adventure-app-29076.firebaseio.com/characters.json';
+
+async function Ldata() {
+
+
+   try {
+
+   let motherArr = [];
+   let eachObject = [];
+
+   let response = await fetch(baseUrl);
+   let data = await response.json();
+   
+         for (let i in data){
+            motherArr.push(data[i]);
+         }
+
+         console.log(motherArr);
+
+         for(var i=0; i<motherArr.length; i++) {
+
+            if(i<4) {
+               eachObject.push(motherArr[i]);
+            }
+         }
+
+         console.log(eachObject);
+
+    }catch(e){
+         console.log("Error! check your connection or url");
+         console.log(e);
+      }
+ 
+}
+
+Ldata();
+
+
+
